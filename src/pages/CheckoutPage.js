8f16eb6262f4639fb9418828da4b1824ed3ac04a
Kennedy <nyongesakennedy06@gmail.com>
2026-07-17 12:47:51 +0300

    Checkout page

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,9 +1,25 @@
 import { Link } from 'react-router-dom';
 
 const CheckoutPage = () => {
+  const cart = [];
+
   return (
-    <div>
-      {/* TODO: Build your checkout page UI */}
+    <div className='checkout'>
+      <Link to = "/" className='back-link2'>--Continue Shopping </Link>
+      <h1 className='checkout-title'>Your Order:</h1>
+
+      {cart.length === 0 ? (
+        <div className='checkout-empty'>
+          <div className='checkout-empty-icon'>🛒</div>
+          <p className='checkout-empty-text'>Your cart is empty. Browse our cafeterias and add some meals!</p>
+          <Link to = "/" className='btn btn--primary btn--lg'>Browse Cafeterias</Link>
+        </div>
+      ) : (
+        <div className="checkout__summary">
+          {/* Real cart items will go here in Step 8 */}
+        </div>
+      )}
+      
     </div>
   );
 };
